15aebd688dd9b49dc21127f11ac6379722e225b9
240638006 <240638006@qq.com>
2012-10-21 02:56:40 +0800

    commit exend springMVC support skip interceptor

--- src/main/java/com/tx/core/springmvc/interceptor/RequestInjectAttributeInterceptor.java
+++ src/main/java/com/tx/core/springmvc/interceptor/RequestInjectAttributeInterceptor.java
@@ -13,21 +13,24 @@ import java.util.Map.Entry;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-
- /**
-  * <向Request中注入部分常量>
-  * <功能详细描述>
-  * 
-  * @author  PengQingyang
-  * @version  [版本号, 2012-10-19]
-  * @see  [相关类/方法]
-  * @since  [产品/模块版本]
-  */
+/**
+ * <向Request中注入部分常量>
+ * <功能详细描述>
+ * 
+ * @author  PengQingyang
+ * @version  [版本号, 2012-10-19]
+ * @see  [相关类/方法]
+ * @since  [产品/模块版本]
+ */
 public class RequestInjectAttributeInterceptor implements HandlerInterceptor {
-
+    
+    private static Logger logger = LoggerFactory.getLogger(RequestInjectAttributeInterceptor.class);
+    
     private Map<String, String> injectAttributes = new HashMap<String, String>();
     
     public static final String CONTEXT_PATH_ATTR_NAME = "contextPath";
@@ -46,7 +49,7 @@ public class RequestInjectAttributeInterceptor implements HandlerInterceptor {
             HttpServletResponse response, Object handler) throws Exception {
         return true;
     }
-
+    
     /**
      * @param request
      * @param response
@@ -59,7 +62,7 @@ public class RequestInjectAttributeInterceptor implements HandlerInterceptor {
             HttpServletResponse response, Object handler,
             ModelAndView modelAndView) throws Exception {
     }
-
+    
     /**
      * @param request
      * @param response
@@ -71,31 +74,40 @@ public class RequestInjectAttributeInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request,
             HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
-        if(injectAttributes == null || injectAttributes.size() == 0){
-            return ;
-        }
-        
+        //设置contextPath
+        logger.info("serverPath {}" + request.getServletPath(),
+                request.getServletPath());
         request.setAttribute(CONTEXT_PATH_ATTR_NAME, request.getContextPath());
         
-        for(Entry<String, String> attrTemp : this.injectAttributes.entrySet()){
-            if(this.isCover){
+        if (injectAttributes == null || injectAttributes.size() == 0) {
+            return;
+        }
+        
+        for (Entry<String, String> attrTemp : this.injectAttributes.entrySet()) {
+            if (logger.isDebugEnabled()) {
+                logger.info("request set Attrubute key: {} value: {} .",
+                        attrTemp.getKey(),
+                        attrTemp.getValue());
+            }
+            
+            if (this.isCover) {
                 request.setAttribute(attrTemp.getKey(), attrTemp.getValue());
                 continue;
             }
             
-            if(request.getAttribute(attrTemp.getKey()) == null){
+            if (request.getAttribute(attrTemp.getKey()) == null) {
                 request.setAttribute(attrTemp.getKey(), attrTemp.getValue());
             }
         }
     }
-
+    
     /**
      * @return 返回 injectAttributes
      */
     public Map<String, String> getInjectAttributes() {
         return injectAttributes;
     }
-
+    
     /**
      * @param 对injectAttributes进行赋值
      */

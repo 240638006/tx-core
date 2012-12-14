0403875a2a7787491affc3a8967f28f28c02317d
240638006 <240638006@qq.com>
2012-12-14 10:08:41 +0800

    xx

--- src/main/java/com/tx/core/exceptions/ErrorCodeConstant.java
+++ src/main/java/com/tx/core/exceptions/ErrorCodeConstant.java
@@ -18,6 +18,7 @@ package com.tx.core.exceptions;
   */
 public interface ErrorCodeConstant {
     
+    /**********************************参数异常*************************************/
     /** 参数异常  */
     String PARAMETER_EXCEPTION = "000-000-001-000";
     
@@ -27,13 +28,22 @@ public interface ErrorCodeConstant {
     /** 参数无效  */
     String PARAMETER_IS_INVALID = "000-000-001-002";
     
+    /**********************************IO异常***************************************/
     /** 资源加载异常 */
     String RESOURCE_LOAD_EXCEPTION = "000-000-002-000";
     
+    /**********************************日志异常**************************************/
     /** 日志异常 */
     String LOG_EXCEPTION = "000-000-003-000";
     
     /** 业务日志记录异常 */
     String LOG_SERVICELOG_EXCEPTION = "000-000-003-100";
+    
+    /**********************************权限异常**************************************/
+    /** 权限容器异常 */
+    String AUTH_CONTEXT_EXCEPTION = "000-000-004-000";
+    
+    /** 权限容器初始化异常 */
+    String AUTH_CONTEXT_INIT_EXCEPTION = "000-000-004-100";
     
 }
